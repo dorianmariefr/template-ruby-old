0da578fd1edfd2569f058956ffd630aa881a16ce
Dorian Marié <dorian@dorianmarie.fr>
2023-01-18 07:33:38 +0100

    add test

--- spec/code/code_spec.rb
+++ spec/code/code_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe "function" do
   it "works with downcase" do
     expect(Code.evaluate("downcase", "{ downcase: 1 }")).to eq(1)
   end
+
+  it "works with nested objects" do
+    expect(
+      Code.evaluate(
+        "items.first.title",
+        ruby: { items: [{ title: "Hello" }] }
+      )
+    ).to eq("Hello")
+  end
 end
